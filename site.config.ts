b8b2e58cf0ff5300210adcc3db319f5ac25232b2
Ath1ant <90352294+Ath1ant@users.noreply.github.com>
2025-05-02 21:36:40 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  // name: null,
-  // domain: null,
-  // author: null,
+  name: null,
+  domain: null,
+  author: null,
 
   // open graph metadata (optional)
   // description: 'Example Next.js Notion Starter Kit Site',
