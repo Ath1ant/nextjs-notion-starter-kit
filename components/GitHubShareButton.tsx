3b6bca4c743f3440361dac7c6e431ffce9e36715
Ath1ant <90352294+Ath1ant@users.noreply.github.com>
2025-05-02 15:07:45 +0300

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.css'
 
 export function GitHubShareButton() {
+  return null; // Просто ничего не рендерим
   return (
     <a
       href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
